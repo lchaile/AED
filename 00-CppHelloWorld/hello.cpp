7ec774d1cf1784ca9045f232b0a8675d07ae7353
Luciana <lchaile@frba.utn.edu.ar>
2026-04-29 17:39:14 -0300

    Update hello.cpp

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -4,6 +4,6 @@ int main() {
     cout << "Hola mundo" << endl;
     cout << "Nombre: Luciana Chaile" << endl;
     cout << "Curso: A.y E.D." << endl;
-    cout << "Grupo: (completar)" << endl;
+    cout << "Grupo: 3" << endl;
     return 0;
 }
