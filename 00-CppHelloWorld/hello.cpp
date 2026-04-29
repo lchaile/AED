60c2817b5bda16a3f712a3a93cd1fce82c0adfc1
Luciana <lchaile@frba.utn.edu.ar>
2026-04-29 17:33:31 -0300

    Create hello.cpp

---
+++ 00-CppHelloWorld/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    cout << "Hola mundo" << endl;
+    cout << "Nombre: Luciana Chaile" << endl;
+    cout << "Curso: A.y E.D." << endl;
+    cout << "Grupo: (completar)" << endl;
+    return 0;
+}
